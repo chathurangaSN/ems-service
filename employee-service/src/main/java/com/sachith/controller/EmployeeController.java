6c43d9b7854ab33ed2deceacb6802464ed6b9d03
chathuranga nanayakkara <sachithmaths@gmail.com>
2019-06-10 20:54:16 +0530

    edited more

--- employee-service/src/main/java/com/sachith/controller/EmployeeController.java
+++ employee-service/src/main/java/com/sachith/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.sachith.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import com.sachith.model.Employee;
 import com.sachith.service.EmployeeService;
 
 @RestController
+@CrossOrigin()
 @RequestMapping(value="/api")
 public class EmployeeController {
 	@Autowired
